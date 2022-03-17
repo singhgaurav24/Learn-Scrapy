741c8d2b2d833c0c9dd82aa143a1948db46c4e19
Gaurav Singh <Gaurav.Singh@AD.MSYSTECHNOLOGIES.COM>
2022-03-17 19:01:58 +0530

    amazon_spider

--- amazon_Scarp/amazon_Scarp/spiders/amazon_spider.py
+++ amazon_Scarp/amazon_Scarp/spiders/amazon_spider.py
@@ -5,17 +5,19 @@ from ..items import AmazonScarpItem
 class AmazonSpiderSpider(scrapy.Spider):
     name = 'amazon_spider'
     start_urls = [
+        # 'https://opensource-demo.orangehrmlive.com/'
         'https://www.amazon.in/s?k=books&i=stripbooks&rh=n%3A976389031&dc&crid=J8NIS8N46TA3&qid=1647517415&rnid'
         '=2684818031&sprefix=books%2Caps%2C1682&ref=sr_nr_p_n_publication_date_1 '
     ]
 
     def parse(self, response):
+        # items = AmazonScarpItem()
+        # product = response.xpath('//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[3]/div/div/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span//text()').get()
+        # product = response.xpath('//*[@id="logInPanelHeading"]//text()').get()
+        product = response.css('h2::text').extract()
+        # items['product_name'] = ''.join(product_name).strip()
+        # items['product'] = product
 
-        items = AmazonScarpItem()
-
-        # product_name = response.xpath('//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[
-        # 3]/div/div/div/div/div/div/div/div[2]/div/div/div[1]/h2').extract()
-        product_name = response.css('.s-access-title').extract()
-        items['Name_Of_Product'] = product_name
-
-        yield items
+        yield {
+            'product_name': product
+        }
